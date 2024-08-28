076439dd562450fde4dd5a1e475b81bd4d8b9f95
Rangga Ayi Pratama <103559445+RLA07@users.noreply.github.com>
2024-08-28 15:36:37 +0700

    Create php.php

---
+++ php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Sederhana PHP</title>
+</head>
+<body>
+    <?php
+        // Mendefinisikan variabel
+        $greeting = "Hello, World!";
+
+        // Menampilkan isi variabel
+        echo "<h1>$greeting</h1>";
+    ?>
+</body>
+</html>
